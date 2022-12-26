a2cf82d679037f532d7f6aeb46f0927965a14ff2
codebysusan <susankhadka708@gmail.com>
2022-12-26 14:40:52 +0530

    Add todo

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+
 const path = require("path");
 
 app.set("view engine", "ejs");
@@ -46,8 +48,8 @@ app.get("/todos/:id", async function (request, response) {
 
 app.post("/todos", async function (request, response) {
   try {
-    const todo = await Todo.addTodo(request.body);
-    return response.json(todo);
+    await Todo.addTodo(request.body);
+    return response.redirect("/");
   } catch (error) {
     console.log(error);
     return response.status(422).json(error);
